87724b81e408b470a0f305a54e88475c18757e8d
IvanSobyna <sobynaivan@gmail.com>
2022-12-25 13:15:53 +0200

    added next week

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let url1 = "http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f";
 
-let apiKey = "b0b34e0501286ae903bab8dde901b6ae";
+let apiKey = "343d33d71141f1623d91c8c8aab91982";
+let lat = 50.4333;
+let lon = 30.5167;
 let cityName = document.querySelector(".city");
 let cityWeather = document.querySelector(".weather-item");
 let weatherClouds = document.querySelector(".weather-clouds");
@@ -9,13 +10,13 @@ let wind = document.querySelector("#wind");
 let humidity = document.querySelector("#hume");
 let date = document.querySelector(".date");
 let input = document.querySelector(".input");
-let date1 = document.querySelector(".date1");
-let day = document.querySelector(".day");
-let celsius = true;
-let fahrenheitBtn;
-let celsiusBtn;
-let units = document.querySelector(".celsiusBtn");
+let buttonSearch = document.querySelector(".buttonSearch");
+let weatherIcon = document.querySelector(".weatherIcon")
+let meteorSituation;
+
 let imgWeather1 = document.querySelector(".imgWeather1");
+let city = "Kyiv";
+let feelsLike = document.querySelector(".feelsLike");
 
 let firstWeekDay = document.querySelector(".firstWeekDay");
 let secondWeekDay = document.querySelector(".secondWeekDay");
@@ -32,123 +33,125 @@ let secondDegree = document.querySelector(".secondDegree");
 let thirdDegree = document.querySelector(".thirdDegree");
 let forthDegree = document.querySelector(".forthDegree");
 
+let dates = new Date();
+let nextWeekNames = dates.getUTCDay();
 
-
-
-
-input.addEventListener("keyup", e => {
-        if (e.key == "Enter" && input.value != ""){
-                requestApi(input.value);
-        }
-});
-
-// fetch(url1)
-//     .then(response => response.json())
-//     .then(function (response) {
-//         cityName.innerHTML = response.name;
-//         cityWeather.innerHTML = Math.round(response.main.temp - 273);
-//         fahrenheitBtn.innerHTML = json.main.temp;
-//         description.innerHTML = response.weather[0].description;
-//         wind.innerHTML = response.wind.speed;
-//         humidity.innerHTML = response.main.humidity;
-//         weatherClouds.innerHTML = '<img src="http://openweathermap.org/img/wn/' + response.weather[0]['icon']+'@2x.png" alt="logo"/>';
-//
-//     });
-
-function requestApi(city){
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-fetch(url)
-    .then(response => response.json())
-    .then(function (response) {
-            cityName.innerHTML = response.name;
-            cityWeather.innerHTML = Math.round(response.main.temp) + '&#176';
-            weatherClouds.innerHTML = '<img src="http://openweathermap.org/img/wn/' + response.weather[0]['icon']+'@2x.png" alt="logo"/>';
-            description.innerHTML = response.weather[0].description;
-            wind.innerHTML = response.wind.speed;
-        humidity.innerHTML = response.main.humidity;
-    })
-}
-
-if (weatherClouds === "Clear"){
-    imgWeather1.src = "img/sunny.png";
-}
-if (weatherClouds === "Clouds"){
-    imgWeather1.src = "img/clouds.png";
-}
-if (weatherClouds === "Snow"){
-    imgWeather1.src = "img/snow.png";
-}
-if (weatherClouds === "Rain"){
-    imgWeather1.src = "img/rain.png";
-}
-
-fetch(url1)
-    .then(response => response.json())
-    .then(function (response) {
-        cityName.innerHTML = response.name;
-        cityWeather.innerHTML = Math.round(response.main.temp - 273) + "&#176";
-        fahrenheitBtn.innerHTML = json.main.temp;
-        description.innerHTML = response.weather[0].description;
-        wind.innerHTML = response.wind.speed;
-        humidity.innerHTML = response.main.humidity + "%";
-        weatherClouds.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + response.weather[0]['icon'] + '@2x.png" alt ="icon"/>';
-
-    });
-
-
-
-
-let now = new Date().toLocaleDateString();
-date.innerHTML = (now);
-
-let dayOfWeek = date.getUTCDay();
-switch (dayOfWeek) {
+switch (nextWeekNames) {
     case 0:
-        firstWeekDay.textContent = "Sunday";
-        secondWeekDay.textContent = "Monday"
-        thirdWeekDay.textContent = "Tuesday"
-        forthWeekDay.textContent = "Wednesday"
-        break;
-    case 1:
         firstWeekDay.textContent = "Monday"
         secondWeekDay.textContent = "Tuesday"
         thirdWeekDay.textContent = "Wednesday"
         forthWeekDay.textContent = "Thursday"
         break;
-
-    case 2:
-        firstWeekDay.textContent = "Tuesday";
+    case 1:
+        firstWeekDay.textContent = "Tuesday"
         secondWeekDay.textContent = "Wednesday"
         thirdWeekDay.textContent = "Thursday"
         forthWeekDay.textContent = "Friday"
         break;
 
-    case 3:
+    case 2:
         firstWeekDay.textContent = "Wednesday"
         secondWeekDay.textContent = "Thursday"
         thirdWeekDay.textContent = "Friday"
         forthWeekDay.textContent = "Saturday"
         break;
 
-    case 4:
+    case 3:
         firstWeekDay.textContent = "Thursday"
         secondWeekDay.textContent = "Friday"
         thirdWeekDay.textContent = "Saturday"
         forthWeekDay.textContent = "Sunday"
         break;
 
-    case 5:
+    case 4:
         firstWeekDay.textContent = "Friday"
         secondWeekDay.textContent = "Saturday"
         thirdWeekDay.textContent = "Sunday"
-        forthWeekDay.textContent = "Monday"
+        forthWeekDay.textContent = "MMonday"
         break;
 
-    case 6:
-        firstWeekDay.textContent = "Saturday";
-        secondWeekDay.textContent = "Sunday"
+    case 5:
+        firstWeekDay.textContent = "SSaturday"
+        secondWeekDay.textContent = "SSunday"
         thirdWeekDay.textContent = "Monday"
         forthWeekDay.textContent = "Tuesday"
         break;
 
+    case 6:
+        firstWeekDay.textContent = "Sunday"
+        secondWeekDay.textContent = "Monday"
+        thirdWeekDay.textContent = "Tuesday"
+        forthWeekDay.textContent = "Wednesday"
+        break;
+
 };
+let now = new Date().toLocaleDateString();
+date.innerHTML = (now);
+
+document.querySelector("input").addEventListener("keypress", showNewCity);
+
+function showNewCity(e) {
+    if (e.key === 'Enter') {
+        city = input.value;
+        showWeather(city)
+    }
+}
+buttonSearch.addEventListener("click", function(){
+    city = input.value;
+    showWeather(city)
+})
+
+function showWeather(e) {
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + `${city}` + "&appid=75e8a972695b454279faf26b7898bfe6";
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            lat = json.coord.lat;
+            lon = json.coord.lon;
+            weatherClouds.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + json.weather[0]['icon'] + '@2x.png" alt ="icon"/>';
+            cityName.innerHTML = json.name;
+            cityWeather.innerHTML = Math.round(json.main.temp - 272) + '&degC';
+            feelsLike.innerHTML = 'Feels likes ' + Math.round(json.main.feels_like - 272) + '&degC';
+            description.innerHTML = json.weather[0].description;
+            wind.innerHTML = 'Wind:' + json.wind.speed + ' m/s';
+            humidity.innerHTML = json.main.humidity + ' %';
+            meteorSituation = json.weather[0].main;
+            if (meteorSituation === 'Rain') {
+                weatherIcon.src = `img/rainyIcon.jpeg`;
+            }
+            if (meteorSituation === 'Clear') {
+                weatherIcon.src = `img/sunIcon.webp`;
+                document.body.classList.add(".clear");
+            }
+            if (meteorSituation === 'Snow') {
+                weatherIcon.src = `img/snowIcon.jpeg`;
+            }
+            if (meteorSituation === 'Clouds') {
+                weatherIcon.src = `img/weatherIcon.webp`;
+                document.body.classList.add(".clouds");
+            }
+        })
+            let url1 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${apiKey}&units=metric`;
+            fetch(url1)
+                .then(response => response.json())
+                .then(json => {
+                        firstWeather.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + json.daily[1].weather[0]['icon'] + '@2x.png" alt ="icon"/>'
+                        firstDegree.innerHTML = Math.round(json.daily[1].temp.day) + "&#176";
+                        secondWeather.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + json.daily[2].weather[0]['icon'] + '@2x.png" alt ="icon"/>'
+                        secondDegree.innerHTML = Math.round(json.daily[2].temp.day) + "&#176";
+                        thirdWeather.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + json.daily[3].weather[0]['icon'] + '@2x.png" alt ="icon"/>'
+                        thirdDegree.innerHTML = Math.round(json.daily[3].temp.day) + "&#176";
+                        forthWeather.innerHTML = '<img src ="http://openweathermap.org/img/wn/' + json.daily[4].weather[0]['icon'] + '@2x.png" alt ="icon"/>'
+                        forthDegree.innerHTML = Math.round(json.daily[4].temp.day) + "&#176";
+                    }
+                )}
+
+showWeather();
+
+
+
+
+
+
+
